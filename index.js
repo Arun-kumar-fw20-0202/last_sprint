6afa9a2d461471228cb231f4ba06e1c6b9863c8c
Arun Kumar ( Katill_Boyy ) <arun07744@gmail.com>
2023-03-27 16:10:16 +0530

    Backdend Done

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const { auth } = require("./middlewares/auth");
 const { userRouter } = require("./routes/users");
 const { postRouter } = require("./routes/post");
 
+app.user(cors());
+
 app.use("/users", userRouter);
 app.use(auth);
 app.use("/post", postRouter);
